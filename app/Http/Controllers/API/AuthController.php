96c4a3b52f1e52f60306f0c26b261726f80ad6f8
Afrozefathima <jsafroze@gmail.com>
2025-06-25 00:43:23 +0400

    commit

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -11,21 +11,18 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+        $credentials = $request->only('email', 'password');
 
-        if (!Auth::attempt($request->only('email', 'password'))) {
+        if (!Auth::guard('web')->attempt($credentials)) {
             return response()->json(['message' => 'Invalid credentials'], 401);
         }
 
-        $user = Auth::user();
+        $user = Auth::guard('web')->user();
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
             'token' => $token,
-            'user' => $user
+            'user' => $user,
         ]);
     }
 
